1dff93f9283a60fc2bf7d74c6c707369a4caaf22
Piotr 123 <piotr.bilka@promerianstartit.pl>
2023-08-20 12:20:14 +0200

    useState,event and simple structure

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-
+import { ReactEventsUseStateMetaData } from './ReactEventsUsedState/router-data';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  ReactEventsUseStateMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
