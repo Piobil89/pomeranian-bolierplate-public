33a4bbd49223ce9c6152c99bda59a1a2ea17c6f3
Piotr 123 <piotr.bilka@promerianstartit.pl>
2023-08-27 14:17:07 +0200

    pierwsza tabelka

---
+++ src/App/Exercises/HtmlCss/HTMLTables/index.jsx
@@ -0,0 +1,44 @@
+import './styles.css';
+export function Exercise() {
+  return (
+    <table>
+      <tr>
+        <th>Plastic</th>
+        <th>Acetone</th>
+        <th>Flame test</th>
+        <th>Heat</th>
+        <th>Crase color</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>no effect</td>
+        <td>Green color</td>
+        <td>Softens</td>
+        <td>None</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>softened</td>
+        <td>no change</td>
+        <td>No change</td>
+        <td>white</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>no effect</td>
+        <td>red color</td>
+        <td>Softenes</td>
+        <td>None</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>no effect</td>
+        <td>Green color</td>
+        <td>Softenes</td>
+        <td>None</td>
+      </tr>
+      <tr></tr>
+      <tr></tr>
+    </table>
+  );
+}
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,7 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as ExcerciseExample } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as BoxModel } from './BoxModel/router-data';
+import { blockRouterMetaData as HTMLTables } from './HTMLTables/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231, BoxModel];
+export const blockRouterMetaData = [ExcerciseExample, BoxModel, HTMLTables];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
