f90c2fb3b66a9ac405d23443c103d8d1f271a34c
Piotr 123 <piotr.bilka@promerianstartit.pl>
2023-08-27 10:18:23 +0200

    RoundImage zmiany

---
+++ src/App/Components/RoundImage/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+export const RoundImage = (props) => {
+  return (
+    <div
+      className="round-image"
+      style={{ backgroundImage: `url('${props.url}')` }}
+    ></div>
+  );
+};
