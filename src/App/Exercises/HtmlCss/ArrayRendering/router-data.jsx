e2f7251e3a7d71bfe5ea905cc68409676d1c9b96
Piotr 123 <piotr.bilka@promerianstartit.pl>
2023-08-27 16:22:32 +0200

    Nowa tabelka

---
+++ src/App/Exercises/HtmlCss/ArrayRendering/router-data.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Exercise } from './index';
+export const blockRouterMetaData = {
+  path: 'array-rendering',
+  date: '27-08-2023',
+  linkLabel: 'Tabelki - style2',
+  blockNo: 14,
+  element: <Exercise />,
+  tabs: ['array', 'HTML', 'JavaScript', 'render', 'react', 'string', 'object'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { blockRouterMetaData as ExcerciseExample } from './Exercise-example-12-1
 import { blockRouterMetaData as BoxModel } from './BoxModel/router-data';
 import { blockRouterMetaData as HTMLTables } from './HTMLTables/router-data';
 import { blockRouterMetaData as TablesTennis } from './TablesTennis/router-data';
+import { blockRouterMetaData as ArrayRendering } from './ArrayRendering/router-data';
 export const blockRouterMetaData = [
   ExcerciseExample,
   BoxModel,
   HTMLTables,
   TablesTennis,
+  ArrayRendering,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
