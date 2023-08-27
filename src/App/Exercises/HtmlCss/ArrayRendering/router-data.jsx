af0615030bd1253223d8fd725e6ee36100734f71
Piotr 123 <piotr.bilka@promerianstartit.pl>
2023-08-27 16:38:29 +0200

    A takie tam

--- src/App/Exercises/HtmlCss/ArrayRendering/router-data.jsx
+++ src/App/Exercises/HtmlCss/ArrayRendering/router-data.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Exercise } from './index';
 export const blockRouterMetaData = {
-  path: 'array-rendering',
+  path: 'array-render-to-table',
   date: '27-08-2023',
-  linkLabel: 'Tabelki - style2',
+  linkLabel: 'Zmiana kolekcji obiektów w tabelkę',
   blockNo: 14,
   element: <Exercise />,
   tabs: ['array', 'HTML', 'JavaScript', 'render', 'react', 'string', 'object'],
